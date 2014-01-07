54b5cdfdb97852b51c2e253efb0312f77708a844
Gunnar Ahlberg <gunnar.ahlberg@tacton.com>
2014-01-07 23:17:47 +0100

    leiningen

---
+++ .lein/profiles.clj
@@ -0,0 +1,12 @@
+{:user
+  {
+  :java-cmd "C:\\libs\\java\\jdk1.6.0_45\\bin\\java.exe"
+   :plugins [
+             [lein-midje-doc "0.0.15"]
+             [lein-midje "3.1.1"]]
+   :dependencies [[slamhound "RELEASE"]]
+   :aliases  {"slamhound" ["run" "-m" "slam.hound"]}
+   
+   }
+  
+ }
